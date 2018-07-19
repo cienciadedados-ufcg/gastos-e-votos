bce9808a2ab387bf0bc84d5723b195c66d26ea33
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-07-19 00:50:08 -0300

    Versão 1.0 para aula

---
+++ README.md
@@ -0,0 +1,7 @@
+# Despesas de campanha viram voto?
+
+Análise da associaçõao entre volume de gastos dos candidatos a deputado federal em 2014 e a quantidade de votos alcançada por eles nessa eleição. Os dados de gastos e votos foram organizados por Tarciso Oliveira e Leandro Balby na disciplina de Ciência de Dados Preditiva. 
+
+O dado pronto para a análise está em `data/gastos_e_votos.csv`. Os mais brutos estão em `data`. O total de votos por uf veio dos arquivos [no repositório do TSE](http://www.tse.jus.br/eleicoes/estatisticas/repositorio-de-dados-eleitorais).
+
+http://setosa.io/ev/ordinary-least-squares-regression/  
---
+++ code/lib.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+
+theme_report <- function(base_size = 11,
+                         strip_text_size = 12,
+                         strip_text_margin = 5,
+                         subtitle_size = 13,
+                         subtitle_margin = 10,
+                         plot_title_size = 16,
+                         plot_title_margin = 10,
+                         ...) {
+    ret <- ggplot2::theme_minimal()
+    ret$strip.text <- ggplot2::element_text(hjust = 0, size=strip_text_size,
+                                            margin=margin(b=strip_text_margin))
+    ret$plot.subtitle <- ggplot2::element_text(hjust = 0, size=subtitle_size,
+                                               margin=margin(b=subtitle_margin))
+    ret$plot.title <- ggplot2::element_text(hjust = 0, size = plot_title_size,
+                                             margin=margin(b=plot_title_margin))
+    ret
+}
+
+import_data <- function(){
+    original = readr::read_csv(here::here("data/eleicoes2014.csv"), 
+                               locale = locale(encoding = "latin1"))
+    eleitorado = read_csv(here::here("data/total_votos_uf.csv")) %>% 
+        select(UF = uf, total_votos_uf = votos)
+    
+    gastos_votos = original %>%
+        select(nome, 
+               UF, 
+               partido,
+               votos,
+               total_despesa,
+               quantidade_doadores, 
+               quantidade_despesas,
+               recursos_de_pessoas_juridicas,
+               idade, 
+               sexo, 
+               grau
+               ) %>% 
+        left_join(eleitorado, by = "UF") %>% 
+        mutate(votos_prop = votos / total_votos_uf)
+    
+    gastos_votos %>% 
+        write_csv(here::here("data/gastos_e_votos.csv"))
+}
+
+read_projectdata <- function(){
+    read_csv(here::here("data/gastos_e_votos.csv"), 
+             col_types = cols(
+                 nome = col_character(),
+                 UF = col_character(),
+                 partido = col_character(),
+                 votos = col_integer(),
+                 total_despesa = col_double(),
+                 quantidade_doadores = col_integer(),
+                 quantidade_despesas = col_integer(),
+                 recursos_de_pessoas_juridicas = col_double(),
+                 idade = col_integer(),
+                 sexo = col_character(),
+                 grau = col_character(),
+                 total_votos_uf = col_integer(),
+                 votos_prop = col_double()
+             ))
+}
